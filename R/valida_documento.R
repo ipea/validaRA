f2c7f7bb783b7835d253d1336c660feb8a8b6a8f
Gustavo Coelho <gutorc@hotmail.com>
2016-12-15 19:16:03 -0200

    CNPJ validation
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

--- R/valida_documento.R
+++ R/valida_documento.R
@@ -12,6 +12,20 @@ verifica_digito_cpf <- function(entrada, digito){
   saida
 }
 
+verifica_digito_cnpj <- function(entrada, digito){
+  entrada <- as.numeric(unlist(strsplit(entrada, "")))
+  digito_verificador <- entrada[digito]
+  vetor_de_validacao <- c(seq(digito-8,2), 9:2, rep(0, 15-digito))
+  resultado <- (sum(entrada * vetor_de_validacao)*10) %% 11
+  if(resultado == 10) resultado <- 0
+  if(digito_verificador == resultado){
+    saida <- TRUE
+  }else{
+    saida <- FALSE
+  }
+  saida
+}
+
 valida_cpf <- function(entrada){
   if(is.integer(entrada) | is.numeric(entrada)){
     entrada <- as.character(entrada)
@@ -28,12 +42,33 @@ valida_cpf <- function(entrada){
   saida
 }
 
+valida_cnpj <- function(entrada){
+  if(is.integer(entrada) | is.numeric(entrada)){
+    entrada <- as.character(entrada)
+  }
+  entrada<- gsub("[^0-9]", "", entrada)
+  if(length(unlist(strsplit(entrada, ""))) != 14){
+    saida <- FALSE
+  }else{
+    saida <- verifica_digito_cnpj(entrada, 13)
+    if(saida == TRUE){
+      saida <- verifica_digito_cnpj(entrada, 14)
+    }
+  }
+  saida
+}
+
 valida_documento <- function(entrada, type = "cpf"){
   num <- length(entrada)
   result <- rep_len(FALSE, length.out = num)
   if(type == "cpf"){
     for(i in 1:num){
       result[i] <- valida_cpf(entrada[i])
+    }
+  }
+  if(type == "cnpj"){
+    for(i in 1:num){
+      result[i] <- valida_cnpj(entrada[i])
     }
   }
   result
--- tests/testthat/testcpf.R
+++ tests/testthat/testcpf.R
@@ -28,3 +28,31 @@ test_that("It is givin a data frame column", {
   expect_equal(result, c(TRUE, TRUE, TRUE))
 })
 
+test_that("CNPJ is giving with especial caracters", {
+  saida <- valida_documento("60.149.443/0001-70", type = "cnpj")
+  expect_equal(saida, TRUE)
+})
+
+test_that("CNPJ is giving with just caracters", {
+  saida <- valida_documento("60149443000170", type = "cnpj")
+  expect_equal(saida, TRUE)
+})
+
+test_that("CNPJ is giving as a numeric", {
+  saida <- valida_documento(60149443000170, type = "cnpj")
+  expect_equal(saida, TRUE)
+})
+
+test_that("A wrong CNPJ is giving", {
+  saida <- valida_documento(60149443000171, type = "cnpj")
+  expect_equal(saida, FALSE)
+})
+
+test_that("It is givin a data frame column", {
+  cnpjs <- c(16784319000195, 11318414000180, 79417547000147)
+  names <- c("BB", "Caixa", "Ipea")
+  df <- data.frame(names = names, cnpjs = cnpjs)
+  result <- valida_documento(df$cnpjs, type = "cnpj")
+  expect_equal(result, c(TRUE, TRUE, TRUE))
+})
+
