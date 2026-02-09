085f81d7360ac74fe85107f58288dde4f6bb32d6
Rodrigo E. S. Borges <rodrigoesborges@gmail.com>
2026-02-09 19:25:25 -0300

    fixes for out of memory writes (CRAN removal)

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // converter_ra_
 SEXP converter_ra_(Rcpp::RObject x);
 RcppExport SEXP _validaRA_converter_ra_(SEXP xSEXP) {
--- src/valida_ra.cpp
+++ src/valida_ra.cpp
@@ -4,23 +4,27 @@
 #include <algorithm>
 #include "utils.h"
 #include <string>
+#include <memory>
 #include <R.h>
 #include <Rdefines.h>
 #include <Rinternals.h>
 
 // [[Rcpp::depends(BH)]]
-// [[Rcpp::plugins(cpp11)]]
 // [[Rcpp::export]]
 SEXP valida_ra(SEXP x, SEXP type, SEXP log){
   SEXP r = PROTECT(Rf_allocVector(LGLSXP, LENGTH(x)));
   SEXP l = PROTECT(Rf_allocVector(INTSXP, LENGTH(x)));
   const char *t = CHAR(STRING_ELT(type,0));
-  Ra *ra = factoryRa(t);
+
+  //Ra *ra = factoryRa(t);
+  std::unique_ptr<Ra> ra(factoryRa(t));
+
   //std::cout << "Is factor: " << is_factor(x) << std::endl;
   if(is_factor(x)){
     //std::cout << "Entrou no factor: " << std::endl;
+    SEXP x_str = PROTECT(Rf_coerceVector(x, STRSXP));
     for(int i = 0; i < LENGTH(x); i++){
-      ra->set_digits(charxp2arrayint(STRING_ELT(x, i),ra->get_size()));
+      ra->set_digits(charxp2arrayint(STRING_ELT(x_str, i),ra->get_size()));
       LOGICAL(r)[i] = ra->validate_generic();
       INTEGER(l)[i] = ra->get_error();
       ra->clear();
@@ -53,7 +57,7 @@ SEXP valida_ra(SEXP x, SEXP type, SEXP log){
     }
 
   }
- // std::cout << "error " <<  ra->get_error() << std::endl;
+  // std::cout << "error " <<  ra->get_error() << std::endl;
   if(LOGICAL(log)[0] == 1){
     Rcpp::DataFrame NDF =
       Rcpp::DataFrame::create(Rcpp::Named("dado")=x,
@@ -67,7 +71,7 @@ SEXP valida_ra(SEXP x, SEXP type, SEXP log){
   return r;
 }
 
-// [[Rcpp::plugins(cpp11)]]
+
 // [[Rcpp::export]]
 void generate_digit(Rcpp::RObject x, SEXP type){
   const char *t = CHAR(STRING_ELT(type,0));
@@ -76,7 +80,9 @@ void generate_digit(Rcpp::RObject x, SEXP type){
     long long * q = (long long *)REAL(x.get__());
     //std::cout << " " << LENGTH(x.get__())  << std::endl;
     for(int i = 0; i < LENGTH(x.get__()); i++){
-      ra->set_digits(bit642arrayint(&q[i], ra->get_size(),ra->sizeRaGenerateLastDigit() ));
+
+      //ra->set_digits(bit642arrayint(&q[i], ra->get_size(),ra->sizeRaGenerateLastDigit() ));
+      ra->set_digits(bit642arrayint(q[i], ra->get_size(),ra->sizeRaGenerateLastDigit() ));
       ra->generate_last_digit();
       q[i] = ra->int2bit64();
       //ra.print_pis();
@@ -85,7 +91,8 @@ void generate_digit(Rcpp::RObject x, SEXP type){
   }else if(x.sexp_type() == REALSXP){
     double * q = REAL((x.get__()));
     for(int i = 0; i < LENGTH(x.get__()); i++){
-      ra->set_digits(double2arrayint(&q[i], ra->get_size(),ra->sizeRaGenerateLastDigit() ));
+      //ra->set_digits(double2arrayint(&q[i], ra->get_size(),ra->sizeRaGenerateLastDigit() ));
+      ra->set_digits(double2arrayint(q[i], ra->get_size(),ra->sizeRaGenerateLastDigit() ));
       ra->generate_last_digit();
       q[i] = ra->int2double();
       //pis.print_pis();
