c1df71efbeecfaceeb2d82043b4c188b6ce2b71d
Rodrigo E. S. Borges <rodrigoesborges@gmail.com>
2026-02-19 17:04:22 -0300

    prints removal

--- src/valida_ra.cpp
+++ src/valida_ra.cpp
@@ -4,11 +4,9 @@
 #include <algorithm>
 #include "utils.h"
 #include <string>
-#include <memory>
 #include <R.h>
 #include <Rdefines.h>
 #include <Rinternals.h>
-#include <R_ext/Print.h>
 
 // [[Rcpp::depends(BH)]]
 // [[Rcpp::export]]
@@ -17,56 +15,50 @@ SEXP valida_ra(SEXP x, SEXP type, SEXP log){
   SEXP l = PROTECT(Rf_allocVector(INTSXP, LENGTH(x)));
   const char *t = CHAR(STRING_ELT(type,0));
 
-  //Ra *ra = factoryRa(t);
-  std::unique_ptr<Ra> ra(factoryRa(t));
+  // Utilização do ponteiro original (raw pointer).
+  // Evita o destrutor automático que causava crash no RStudio (Heap Corruption)
+  Ra *ra = factoryRa(t);
 
-  //std::cout << "Is factor: " << is_factor(x) << std::endl;
   if(is_factor(x)){
-    //std::cout << "Entrou no factor: " << std::endl;
+    // CORREÇÃO CRÍTICA PARA O CRAN: Fatores são INTSXP internamente.
+    // É obrigatório coagir para STRSXP antes de usar STRING_ELT.
     SEXP x_str = PROTECT(Rf_coerceVector(x, STRSXP));
-    for(int i = 0; i < LENGTH(x); i++){
-      ra->set_digits(charxp2arrayint(STRING_ELT(x_str, i),ra->get_size()));
 
-      // Antes de validar
-      Rprintf("Validando...\n");
-
+    for(int i = 0; i < LENGTH(x_str); i++){
+      ra->set_digits(charxp2arrayint(STRING_ELT(x_str, i), ra->get_size()));
       LOGICAL(r)[i] = ra->validate_generic();
       INTEGER(l)[i] = ra->get_error();
-
-      // Antes de limpar
-      Rprintf("Limpando...\n");
-
       ra->clear();
     }
+    UNPROTECT(1); // Desprotege o x_str
 
   }else if(TYPEOF(x) == STRSXP){
     for(int i = 0; i < LENGTH(x); i++){
-      ra->set_digits(charxp2arrayint(STRING_ELT(x, i),ra->get_size()));
+      ra->set_digits(charxp2arrayint(STRING_ELT(x, i), ra->get_size()));
       LOGICAL(r)[i] = ra->validate_generic();
       INTEGER(l)[i] = ra->get_error();
       ra->clear();
-
     }
 
   }else if(TYPEOF(x) == REALSXP && is_bit64(x)){
     long long * q = (long long *)REAL(x);
     for(int i = 0; i < LENGTH(x); i++){
-      ra->set_digits(bit642arrayint(q[i], ra->get_size(),ra->sizeRaValidate() ));
-      LOGICAL(r)[i] = ra->validate_generic();
-      INTEGER(l)[i] = ra->get_error();
-      ra->clear();
-    }
-  }else if(TYPEOF(x) == REALSXP){
-    double * q = REAL(x);
-    for(int i = 0; i < LENGTH(x); i++){
-      ra->set_digits(double2arrayint(q[i], ra->get_size(),ra->sizeRaValidate() ));
+      ra->set_digits(bit642arrayint(q[i], ra->get_size(), ra->sizeRaValidate() ));
       LOGICAL(r)[i] = ra->validate_generic();
       INTEGER(l)[i] = ra->get_error();
       ra->clear();
     }
 
+  }else if(TYPEOF(x) == REALSXP){
+    double * q = REAL(x);
+    for(int i = 0; i < LENGTH(x); i++){
+      ra->set_digits(double2arrayint(q[i], ra->get_size(), ra->sizeRaValidate() ));
+      LOGICAL(r)[i] = ra->validate_generic();
+      INTEGER(l)[i] = ra->get_error();
+      ra->clear();
+    }
   }
-  // std::cout << "error " <<  ra->get_error() << std::endl;
+
   if(LOGICAL(log)[0] == 1){
     Rcpp::DataFrame NDF =
       Rcpp::DataFrame::create(Rcpp::Named("dado")=x,
@@ -76,58 +68,41 @@ SEXP valida_ra(SEXP x, SEXP type, SEXP log){
     UNPROTECT(2);
     return NDF.get__();
   }
+
   UNPROTECT(2);
   return r;
 }
 
-
 // [[Rcpp::export]]
 void generate_digit(Rcpp::RObject x, SEXP type){
   const char *t = CHAR(STRING_ELT(type,0));
+
+  // Utilização do ponteiro original (raw pointer)
   Ra *ra = factoryRa(t);
+
   if(x.sexp_type() == REALSXP && is_bit64(x.get__())){
     long long * q = (long long *)REAL(x.get__());
-    //std::cout << " " << LENGTH(x.get__())  << std::endl;
     for(int i = 0; i < LENGTH(x.get__()); i++){
-
-      //ra->set_digits(bit642arrayint(&q[i], ra->get_size(),ra->sizeRaGenerateLastDigit() ));
-      ra->set_digits(bit642arrayint(q[i], ra->get_size(),ra->sizeRaGenerateLastDigit() ));
+      ra->set_digits(bit642arrayint(q[i], ra->get_size(), ra->sizeRaGenerateLastDigit() ));
       ra->generate_last_digit();
       q[i] = ra->int2bit64();
-      //ra.print_pis();
     }
-    //std::cout << "falskjflakjfslaksjflakjfslaksfjlakf" << std::endl;
+
   }else if(x.sexp_type() == REALSXP){
     double * q = REAL((x.get__()));
     for(int i = 0; i < LENGTH(x.get__()); i++){
-      //ra->set_digits(double2arrayint(&q[i], ra->get_size(),ra->sizeRaGenerateLastDigit() ));
-      ra->set_digits(double2arrayint(q[i], ra->get_size(),ra->sizeRaGenerateLastDigit() ));
+      ra->set_digits(double2arrayint(q[i], ra->get_size(), ra->sizeRaGenerateLastDigit() ));
       ra->generate_last_digit();
       q[i] = ra->int2double();
-      //pis.print_pis();
     }
   }
 
   if(x.sexp_type() == STRSXP){
     for(int i = 0; i < LENGTH(x.get__()); i++){
-      Rprintf("Iteracao %d: Iniciando...\n", i);
-
-      // Antes da conversão
-      Rprintf("Chamando conversao...\n");
-      // Antes da conversão
-      Rprintf("Chamando conversao...\n");
-      int* digitos = charxp2arrayint(STRING_ELT(x, i), ra->get_size());
-
-      // Antes de setar no objeto
-      Rprintf("Setando digitos...\n");
-      ra->set_digits(digitos);
-
+      ra->set_digits(charxp2arrayint(STRING_ELT(x.get__(), i), ra->get_size()));
       ra->generate_last_digit();
-      //pis.print_pis();
       SEXP q = Rf_mkChar(ra->int2char());
-      //std::cout << TYPEOF(q) << std::endl;
       SET_STRING_ELT(x.get__(), i, q);
-
     }
   }
 }
